d69fc988c10113aa0754e159ab0a6eadf804cd55
4hiziri <meirvg@gmail.com>
2017-05-08 16:31:14 +0900

    define json-entities to all struct

--- src/cl-mstdn.lisp
+++ src/cl-mstdn.lisp
@@ -36,7 +36,10 @@
 
 (defun auth-header (token)
   (list (cons "Authorization"
-	      (format nil "Bearer ~A" (cdr (assoc :access--token token))))))
+	      (format nil "Bearer ~A" (access-token-access-token token)))))
+
+(defun getalist (key alist)
+  (cdr (assoc key alist)))
 
 ;;;; struct
 ;; Should I use Local-time?
@@ -53,12 +56,12 @@
   created-time)
 
 (defstruct Account
-  account-id
+  id
   user-name
   acct
   display-name
   locked-p
-  created_time
+  created-time
   followers-count
   following-count
   note
@@ -74,7 +77,7 @@
   (website ""))
 
 (defstruct Attachment
-  attach-id
+  id
   type
   url
   remote-url
@@ -104,25 +107,25 @@
   url
   user-name
   acc
-  id)
+  account-id)
 
 (defstruct Notification
-  note-id
+  id
   type
   created-time
   account
   status)
 
 (defstruct Relationship
-  rel-id
-  followings
-  followed_by
-  block
-  mute
-  requested)
+  id
+  following-p
+  followed-by-p
+  block-p
+  mute-p
+  requested-p)
 
 (defstruct Report
-  report-id
+  id
   action-taken)
 
 (defstruct Result
@@ -131,7 +134,7 @@
   hashtags)
 
 (defstruct Status
-  status-id
+  id
   uri
   url
   account
@@ -158,18 +161,136 @@
 
 ;;; json-struct
 (defun json-client-token (json-alist)
-  (make-client-token :id (cdr (assoc :id json-alist))
-		     :redirect-uri (cdr (assoc :redirect--uri json-alist))
-		     :client-id (cdr (assoc :client--id json-alist))
-		     :secret-key (cdr (assoc :client--secret json-alist))))
+  (make-client-token :id (getalist :id json-alist)
+		     :redirect-uri (getalist :redirect--uri json-alist)
+		     :client-id (getalist :client--id json-alist)
+		     :secret-key (getalist :client--secret json-alist)))
 
 (defun json-access-token (json-alist)  
-  (make-access-token :access-token (cdr (assoc :access--token json-alist))
-		     :token-type (cdr (assoc :token--type json-alist))
+  (make-access-token :access-token (getalist :access--token json-alist)
+		     :token-type (getalist :token--type json-alist)
 		     ;; to symbol ?
-		     :scope (split-sequence:split-sequence #\space (cdr (assoc :scope json-alist)))
-		     :created-time (cdr (assoc :created--at json-alist))))
+		     :scope (split-sequence:split-sequence #\space (getalist :scope json-alist))
+		     :created-time (getalist :created--at json-alist)))
 
+(defun json-account (json-alist)
+  (if json-alist
+      (make-account :id (getalist :id json-alist)
+		    :user-name (getalist :username json-alist)
+		    :acct (getalist :acct json-alist)
+		    :display-name (getalist :display--name json-alist)
+		    :locked-p (getalist :locked json-alist) ;; :TODO test locked state
+		    :created-time (getalist :created--at json-alist)
+		    :followers-count (getalist :followers--count json-alist)
+		    :following-count (getalist :following--count json-alist)
+		    :note (getalist :note json-alist)
+		    :url (getalist :url json-alist)
+		    :avatar (getalist :avatar json-alist)
+		    :avatar-static (getalist :avatar--static json-alist)
+		    :header (getalist :header json-alist)
+		    :header-static (getalist :header--static json-alist))
+      nil))
+
+(defun json-app (json-alist)
+  (if json-alist
+      (make-app :name (getalist :name json-alist)
+		:website (getalist :website json-alist))
+      nil))
+
+(defun json-attachment (json-alist)
+  (if json-alist      
+      (make-attachment :id (getalist :id json-alist)
+		       :type (getalist :type json-alist)
+		       :url (getalist :url json-alist)
+		       :remote-url (getalist :remote--url json-alist)
+		       :preview-url (getalist :preview--url json-alist)
+		       :text-url (getalist :text--url json-alist))
+      nil))
+
+(defun json-card (json-alist)
+  (make-card :url (getalist :url json-alist)
+	     :title (getalist :title json-alist)
+	     :description (getalist :description json-alist)
+	     :image (getalist :image json-alist)))
+
+(defun json-context (json-alist)
+  (make-context :ancestors (mapcar (lambda (x) (json-status x))
+				   (getalist :ancestors json-alist))
+		:descendants (mapcar (lambda (x) (json-status x))
+				     (getalist :descendants json-alist))))
+
+(defun json-error (json-alist)
+  (make-mstdn-error :mstdn-error (getalist :error json-alist)))
+
+(defun json-instance (json-alist)
+  (make-mstdn-instance :uri (getalist :uri json-alist)
+		       :title (getalist :title json-alist)
+		       :description (getalist :description json-alist)
+		       :email (getalist :email json-alist)))
+
+(defun json-mention (json-alist)
+  (make-mention :url (getalist :url json-alist)
+		:user-name (getalist :username json-alist)
+		:acc (getalist :acct json-alist)
+		:account-id (getalist :id json-alist)))
+
+(defun json-notification (json-alist)
+  (make-notification :id (getalist :id json-alist)
+		     :type (getalist :type json-alist)
+		     :created-time (getalist :created--at json-alist)
+		     :account (json-account (getalist :account json-alist))
+		     :status (json-status (getalist :status json-alist))))
+
+(defun json-relation (json-alist)
+  (make-relationship :id (getalist :id json-alist)
+		     :following-p (getalist :following json-alist)
+		     :followed-by-p (getalist :followed--by json-alist)
+		     :block-p (getalist :blocking json-alist)
+		     :mute-p (getalist :muting json-alist)
+		     :requested-p (getalist :requested json-alist)))
+
+(defun json-report (json-alist)
+  (make-report :id (getalist :id json-alist)
+	       :action-taken (getalist :action--taken json-alist)))
+
+(defun json-result (json-alist)
+  (make-result :accounts (mapcar (lambda (x) (json-account x))
+				 (getalist :accounts json-alist))
+	       :statuses (mapcar (lambda (x) (json-account x))
+				 (getalist :statuses json-alist))
+	       :hashtags (getalist :hashtags json-alist))) ;; :TODO extract struct?
+
+(defun json-tag (json-alist)
+  (if json-alist
+      (make-tag :name (getalist :name json-alist)
+		:url (getalist :url json-alist))
+      nil))
+
+
+
+(defun json-status (json-alist)
+  (if json-alist
+      (make-status :id (getalist :id json-alist)
+		   :uri (getalist :uri json-alist)
+		   :url (getalist :url json-alist)
+		   :account (json-account (getalist :account json-alist))
+		   :reply-to-status (getalist :in--reply--to--account-id json-alist)
+		   :reply-to-account (getalist :in--reply--to--id json-alist)
+		   :reblog-status (json-status (getalist :reblog json-alist))
+		   :content (getalist :content json-alist)
+		   :created-time (getalist :created--at json-alist)
+		   :reblogs-count (getalist :reblogs--count json-alist)
+		   :favourites-count (getalist :favourites--count json-alist)
+		   :reblogged (getalist :reblogged json-alist)
+		   :favourited (getalist :favourited json-alist)
+		   :sensitive (getalist :sensitive json-alist) ;; TODO what is returned
+		   :spoiler-text (getalist :spoiler--text json-alist)
+		   :visibility (getalist :visibility json-alist)
+		   :media-attach (mapcar #'json-attachment (getalist :media--attachments json-alist))
+		   :mentions (mapcar #'json-mention (getalist :mentions json-alist))
+		   :tags (mapcar #'json-tag (getalist :tags json-alist))
+		   :app (getalist :application json-alist))
+      nil))
 ;; TODO make functions return these struct
 
 ;;; public
@@ -188,8 +309,8 @@
   (json-access-token
    (json:decode-json-from-string
     (dex:post (instance-url instance "/oauth/token")
-	      :content `(("client_id" . ,(cdr (assoc :client--id token)))
-			 ("client_secret" . ,(cdr (assoc :client--secret token)))
+	      :content `(("client_id" . ,(client-token-id token))
+			 ("client_secret" . ,(client-token-secret-key token))
 			 ("grant_type" . "password")
 			 ("username" . ,username)
 			 ("password" . ,password)
@@ -199,46 +320,71 @@
 ;;; accounts
 @export
 (defun fetch-account (instance token usr-id)
-  (json:decode-json-from-string
-   (dex:get (instance-url instance "/api/v1/accounts/" (princ-to-string usr-id))
-	    :headers (auth-header token))))
+  (json-account
+   (json:decode-json-from-string
+    (dex:get (instance-url instance "/api/v1/accounts/" (princ-to-string usr-id))
+	     :headers (auth-header token)))))
 
 @export
 (defun get-current-user (instance token)
-  (json:decode-json-from-string
-   (dex:get (instance-url instance "/api/v1/accounts/verify_credentials")
-		:headers (auth-header token))))
+  (json-account   
+   (json:decode-json-from-string
+    (dex:get (instance-url instance "/api/v1/accounts/verify_credentials")
+	     :headers (auth-header token)))))
 
 ;; :TODO unifying by it's item?
+;; :TODO content -> key
 @export
 (defun update-current-user (instance token content)
-  "content:
+  "content is a-list
+content:
 display_name = The name to display in the user's profile
 note = A new biography for the user
 avatar = A base64 encoded image to display as the user's avatar 
 (e.g. data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAUoAAADrCAYAAAA...)
 header = A base64 encoded image to display as the user's header image 
 (e.g. data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAUoAAADrCAYAAAA...)
-"
+      "
+(json-account
+ (json:decode-json-from-string
   (dex:request (instance-url instance "/api/v1/accounts/update_credentials")
 	       :method :patch
 	       :headers (auth-header token)
-	       :content content))
+	       :content content))))
 
 ;; :WARN max_id and since_id do wired behaviour
+(defun get-follow-info (instance token method uid querys)
+  (mapcar (lambda (x) (json-account x))
+	    (json:decode-json-from-string
+	     (dex:get (instance-url instance
+				    "/api/v1/accounts/"
+				    (princ-to-string uid)
+				    "/"
+				    method
+				    querys) ;; rev?
+		      :headers (auth-header token)))))
+
 @export
-(defun get-followers (instance token user-id &key
-					       (max-id nil max-id-p)
-					       (since-id nil since-id-p)
-					       (limit 0 limit-p))
-  (let ((querys nil)
-	(uid (princ-to-string user-id)))
+(defun get-followers (instance token uid &key
+					   (max-id nil max-id-p)
+					   (since-id nil since-id-p)
+					   (limit 0 limit-p))
+  (let ((querys nil))
     (push-pair "max_id" (princ-to-string max-id) max-id-p querys)
     (push-pair "since_id" (princ-to-string since-id) since-id-p querys)
     (push-pair "limit" (princ-to-string limit) limit-p querys)    
-    (json:decode-json-from-string
-     (dex:get (instance-url instance "/api/v1/accounts/" uid "/following" (get-query querys)) ;; rev?
-	      :headers (auth-header token)))))
+    (get-follow-info instance token "followers" uid (get-query querys))))
+
+@export
+(defun get-followings (instance token uid &key
+					    (max-id nil max-id-p)
+					    (since-id nil since-id-p)
+					    (limit 0 limit-p))
+  (let ((querys nil))
+    (push-pair "max_id" (princ-to-string max-id) max-id-p querys)
+    (push-pair "since_id" (princ-to-string since-id) since-id-p querys)
+    (push-pair "limit" (princ-to-string limit) limit-p querys)
+    (get-follow-info instance token "following" uid (get-query querys))))
 
 ;; TODO query
 ;; TODO another user
@@ -256,15 +402,16 @@ header = A base64 encoded image to display as the user's header image
     (push-pair "exclude_replies" exclude-rep rep-p querys) ;; need test. It will take t or nil
     (push-pair "max_id" (princ-to-string max-id) max-p querys)    
     (push-pair "since_id" (princ-to-string since-id) since-p querys)
-    (push-pair "limit" (princ-to-string limit) limit-p querys)    
-    (json:decode-json-from-string
-     (dex:get (strings "https://" instance "/api/v1/accounts/" uid "/statuses")
-	      :headers (auth-header token)))))
+    (push-pair "limit" (princ-to-string limit) limit-p querys)
+    (mapcar (lambda (x) (json-status x))
+	    (json:decode-json-from-string
+	     (dex:get (strings "https://" instance "/api/v1/accounts/" uid "/statuses")
+		      :headers (auth-header token))))))
 
 ;; return relationships
 @export
 (defun account-method-account (instance token account method)
-  (let ((user-id (princ-to-string (cdr (assoc :id account)))))
+  (let ((user-id (princ-to-string (getalist :id account))))
     (json:decode-json-from-string
      (dex:post (instance-url instance "/api/v1/accounts/" (format nil "~A/~A" user-id method))
 	       :headers (auth-header token)))))
@@ -298,7 +445,7 @@ header = A base64 encoded image to display as the user's header image
 @export
 (defun account-relations (instance token &optional (account nil account-p))
   (let ((user-id (if account-p ;; :TODO duplicated
-		     (princ-to-string (cdr (assoc :id account)))
+		     (princ-to-string (getalist :id account))
 		     "")))
     (json:decode-json-from-string
      (dex:get (instance-url instance "/api/v1/accounts/relationships" (if account-p
